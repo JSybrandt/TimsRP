07dcab7ebf82bd64495736301db9d794d66592ca
Darkfire16 <gmodman12@gmail.com>
2015-09-10 15:34:38 -0400

    Committing, minus the the obj folder.

---
+++ TimsRP/TimsRP/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TimsRP
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
