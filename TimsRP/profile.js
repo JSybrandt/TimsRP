cbb10da4bc217f5dad08f11dffa6f549ac1f5b95
ZARD <zaredsoutdoors@gmail.com>
2015-10-27 00:37:07 -0400

    Finished JS for profile page.

--- TimsRP/profile.js
+++ TimsRP/profile.js
@@ -26,7 +26,7 @@ function validateGenInfo() {
 		alert("Please enter a valid US zip code");
 	}
 	
-	
+	alert("Your profile has been updated!");
 	//Return
 	return false;
 }
@@ -43,4 +43,45 @@ function validateEmail(email) {
   
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+
+function confirmPassword(){
+	var pass = document.forms["PassForm"]["newPass"].value;
+	var confPass = document.forms["PassForm"]["confirmPass"].value;
+	
+	if(pass != confPass){
+		alert("New password does not match!");
+	}
+}
+
+//ValidateFileUpload taken from the author newbieinjavaversion2 from the following URL:
+//http://stackoverflow.com/questions/21396279/display-image-and-validation-of-image-extension-before-uploading-file-using-java
+function ValidateFileUpload() {
+	var fuData = document.getElementById('fileChooser');
+    var FileUploadPath = fuData.value;
+
+	//To check if user upload any file
+    if (FileUploadPath == '') {
+        alert("Please upload an image");
+
+        } else {
+        var Extension = FileUploadPath.substring(FileUploadPath.lastIndexOf('.') + 1).toLowerCase();
+
+	//The file uploaded is an image
+
+	if (Extension == "gif" || Extension == "png" || Extension == "bmp" || Extension == "jpeg" || Extension == "jpg") {
+		// To Display
+		if (fuData.files && fuData.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function(e) {$('#avatar').attr('src', e.target.result);}
+			reader.readAsDataURL(fuData.files[0]);
+			}
+    } 
+
+	//The file upload is NOT an image
+	else {
+			alert("Photo only allows file types of GIF, PNG, JPG, JPEG and BMP. ");
+
+		}
+	}
 }
