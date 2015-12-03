b57ac25e5396216d64e4e2622bdc1cf722099f7f
Stuart Bowman <stuartsoft2012@gmail.com>
2015-12-03 13:44:43 -0500

    RequestMembership page is now working properly!

--- TimsRP/requestJoinGame.js
+++ TimsRP/requestJoinGame.js
@@ -1,7 +1,23 @@
-function addRequest(){
-	var g = 'asdf';
-	var u = 'stuartsoft';
-	var stuff = {gameid: g, userid: u};
-	$.ajax(stuff);
-	return true;
+var RJG = {
+	init: function(){
+		//newGame.UpdateEventListeners();
+		//document.getElementById("requestRP").addEventListener("click", function() {RJG.requestgame()}, false);
+	},
+
+
+	requestgame: function(gid, uid){
+		//client side form validation before submitting
+		
+		var jqxhr = $.post( "dbEdits/addPlayerToRequest.php",{gameid:gid, userid: uid });
+		
+		jqxhr.done(function(msg){
+			console.log(msg);
+			
+			window.location = 'requestJoinGame.php';
+		});
+		jqxhr.fail(function(){alert("Error requesting game.")});
+		
+	}
 }
+
+RJG.init();
